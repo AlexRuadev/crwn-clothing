e04d09256cab1df7792e5c30aaa5f5aca4dd6345
Alex R <alexruadev@gmail.com>
2019-12-21 19:47:16 +0700

    added cartIcon and cart-dropdown components to our application, also implemented cart reducer

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -3,13 +3,15 @@ import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 import { auth } from '../../firebase/firebase.utils';
+import CartIcon from '../cart-icon/cart-icon.component';
+import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 
 import './header.styles.scss';
 
 // functionnal component
-const Header = ({ currentUser }) => (
+const Header = ({ currentUser, hidden }) => (
 	<div className="header">
 		{/* our logo link to homepage on click  */}
 		<Link className="logo-container" to="/">
@@ -31,12 +33,16 @@ const Header = ({ currentUser }) => (
 					SIGN IN
 				</Link>
 			)}
+			<CartIcon />
 		</div>
+		{hidden ? null : <CartDropdown />}
 	</div>
 );
 
-const mapStateToProps = (state) => ({
-	currentUser: state.user.currentUser
+// this is how to destructure nested values
+const mapStateToProps = ({ user: { currentUser }, cart: { hidden } }) => ({
+	currentUser,
+	hidden
 });
 
 export default connect(mapStateToProps)(Header);
