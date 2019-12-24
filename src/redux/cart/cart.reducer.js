bc403ba851d85bde3a217a4b4ae5632f29eeb565
Alex R <alexruadev@gmail.com>
2019-12-24 21:59:16 +0700

    updating our application with reselect, replaced cart selectors with memorized selectors

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -2,8 +2,8 @@ import CartActionTypes from './cart.types';
 import { addItemToCart } from './cart.utils';
 
 const INITIAL_STATE = {
-	hidden: true,
-	cartItems: []
+	hidden    : true,
+	cartItems : []
 };
 
 const cartReducer = (state = INITIAL_STATE, action) => {
@@ -11,13 +11,13 @@ const cartReducer = (state = INITIAL_STATE, action) => {
 		case CartActionTypes.TOGGLE_CART_HIDDEN:
 			return {
 				...state,
-				hidden: !state.hidden
+				hidden : !state.hidden
 			};
 		case CartActionTypes.ADD_ITEM:
 			return {
 				...state,
 				// we spread our array values and any additional values added at the end will appear at the end of our array
-				cartItems: addItemToCart(state.cartItems, action.payload)
+				cartItems : addItemToCart(state.cartItems, action.payload)
 			};
 		default:
 			return state;
