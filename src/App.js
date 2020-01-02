955f654e9d0fcaaa8217063e6986055ff7f06cc5
Alex R <alexruadev@gmail.com>
2020-01-02 20:22:24 +0700

    added checkout page, checkout item component and updated cart to use selectors from reselect

--- src/App.js
+++ src/App.js
@@ -1,20 +1,26 @@
 import React from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
 
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+import CheckoutPage from './pages//checkout/checkout.component';
+
 import Header from './components/header/header.component';
+
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
+
 import { setCurrentUser } from './redux/user/user.actions';
+import { selectCurrentUser } from './redux/user/user.selectors';
 
 class App extends React.Component {
 	unsubscribeFromAuth = null;
 
-	componentDidMount() {
+	componentDidMount () {
 		const { setCurrentUser } = this.props;
 
 		this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
@@ -24,35 +30,40 @@ class App extends React.Component {
 
 				userRef.onSnapshot((snapShot) => {
 					setCurrentUser({
-						currentUser: {
-							id: snapShot.id,
+						currentUser : {
+							id : snapShot.id,
 							...snapShot.data()
 						}
 					});
 				});
-			} else {
+			}
+			else {
 				setCurrentUser(userAuth);
 			}
 		});
 	}
 
 	// this will close the subscription
-	componentWillUnmount() {
+	componentWillUnmount () {
 		this.unsubscribeFromAuth();
 	}
 
-	render() {
+	render () {
 		return (
 			<div>
 				{/* currentUser to check if log or not */}
 				<Header />
 				<Switch>
-					<Route exact path="/" component={HomePage} />
-					<Route path="/shop" component={ShopPage} />
+					<Route exact path='/' component={HomePage} />
+					<Route path='/shop' component={ShopPage} />
+					<Route path='/checkout' component={CheckoutPage} />
 					<Route
 						exact
-						path="/signIn"
-						render={() => (this.props.currentUser ? <Redirect to="/" /> : <SignInAndSignUpPage />)}
+						path='/signIn'
+						render={() =>
+
+								this.props.currentUser ? <Redirect to='/' /> :
+								<SignInAndSignUpPage />}
 					/>
 				</Switch>
 			</div>
@@ -60,12 +71,12 @@ class App extends React.Component {
 	}
 }
 
-const mapStateToProps = ({ user }) => ({
-	currentUser: user.currentUser
+const mapStateToProps = createStructuredSelector({
+	currentUser : selectCurrentUser
 });
 
 const mapDispatchToProps = (dispatch) => ({
-	setCurrentUser: (user) => dispatch(setCurrentUser(user))
+	setCurrentUser : (user) => dispatch(setCurrentUser(user))
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/redux/cart/cart.selectors.js
+++ src/redux/cart/cart.selectors.js
@@ -2,10 +2,39 @@ import { createSelector } from 'reselect';
 
 const selectCart = (state) => state.cart;
 
-const selectUser = (state) => state.cart;
+export const selectCartItems = createSelector(
+	[
+		selectCart
+	],
+	(cart) => cart.cartItems
+);
 
-export const selectCartItems = createSelector([ selectCart ], (cart) => cart.cartItems);
+export const selectCartHidden = createSelector(
+	[
+		selectCart
+	],
+	(cart) => cart.hidden
+);
 
-export const selectCartItemsCount = createSelector([ selectCartItems ], (cartItems) =>
-	cartItems.reduce((accumulatedQuantity, cartItem) => accumulatedQuantity + cartItem.quantity, 0)
+export const selectCartItemsCount = createSelector(
+	[
+		selectCartItems
+	],
+	(cartItems) =>
+		cartItems.reduce(
+			(accumalatedQuantity, cartItem) => accumalatedQuantity + cartItem.quantity,
+			0
+		)
+);
+
+export const selectCartTotal = createSelector(
+	[
+		selectCartItems
+	],
+	(cartItems) =>
+		cartItems.reduce(
+			(accumalatedQuantity, cartItem) =>
+				accumalatedQuantity + cartItem.quantity * cartItem.price,
+			0
+		)
 );
