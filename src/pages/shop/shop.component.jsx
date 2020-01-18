91042e5e916ef0a573c23e351c7294685ca80a6d
Alex R <alexruadev@gmail.com>
2020-01-18 19:34:40 +0700

    created user and cart sagas for sign in, sign up and sign in with google

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -2,57 +2,31 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 import { connect } from 'react-redux';
 
-import { firestore, convertCollectionsSnapshotToMap } from '../../firebase/firebase.utils.js';
+import { fetchCollectionsStart } from '../../redux/shop/shop.actions';
 
-import { updateCollections } from '../../redux/shop/shop.actions';
-
-import WithSpinner from '../../components/with-spinner/with-spinner.component';
-
-import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
-import CollectionPage from '../collection/collection.component';
-
-const CollectionsOverviewWithSpinner = WithSpinner(CollectionsOverview);
-const CollectionPageWithSpinner = WithSpinner(CollectionPage);
+import CollectionsOverviewContainer from '../../components/collections-overview/collections-overview.container';
+import CollectionPageContainer from '../collection/collection.container';
 
 class ShopPage extends React.Component {
-	state = {
-		loading : true
-	};
 
-	unsubscribeFromSnapshot = null;
+  componentDidMount() {
+    const { fetchCollectionsStart } = this.props;
 
-	componentDidMount () {
-		const { updateCollections } = this.props;
-		const collectionRef = firestore.collection('collections');
+    fetchCollectionsStart();
+  }
+  render() {
+    const { match } = this.props;
 
-		collectionRef.get().then(snapshot => {
-			const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
-			updateCollections(collectionsMap);
-			this.setState({ loading: false });
-		});
-	}
+    return ( <div className='shop-page'>
 
-	render () {
-		const { match } = this.props;
-		const { loading } = this.state;
-		return (
-			<div className='shop-page'>
-				<Route
-					exact
-					path={`${match.path}`}
-					render={props => <CollectionsOverviewWithSpinner isLoading={loading} {...props} />}
-				/>
-				<Route
-					path={`${match.path}/:collectionId`}
-					render={props => <CollectionPageWithSpinner isLoading={loading} {...props} />}
-				/>
-			</div>
-		);
-	}
+      <Route exact="exact" path={`${ match.path }`} component={CollectionsOverviewContainer}/>
+      <Route path={`${ match.path }/:collectionId`} component={CollectionPageContainer}/>
+
+    </div> );
+  }
 }
 
-const mapDispatchToProps = dispatch => ({
-	updateCollections : collectionsMap => dispatch(updateCollections(collectionsMap))
-});
-
-export default connect(null, mapDispatchToProps)(ShopPage);
+const mapDispatchToProps = dispatch => ( {
+  fetchCollectionsStart: () => dispatch( fetchCollectionsStart() )
+} );
+export default connect( null, mapDispatchToProps )( ShopPage );
--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,39 +1,45 @@
-import CartActionTypes from './cart.types';
-import { addItemToCart, removeItemFromCart } from './cart.utils';
+import CartActionTypes from "./cart.types";
+import {addItemToCart, removeItemFromCart} from "./cart.utils";
 
 const INITIAL_STATE = {
-	hidden    : true,
-	cartItems : []
+  hidden: true,
+  cartItems: []
 };
 
 const cartReducer = (state = INITIAL_STATE, action) => {
-	switch (action.type) {
-		case CartActionTypes.TOGGLE_CART_HIDDEN:
-			return {
-				...state,
-				hidden : !state.hidden
-			};
-		case CartActionTypes.ADD_ITEM:
-			return {
-				...state,
-				// we spread our array values and any additional values added at the end will appear at the end of our array
-				cartItems : addItemToCart(state.cartItems, action.payload)
-			};
-		case CartActionTypes.REMOVE_ITEM:
-			return {
-				// spreading the state cause we want all the properties
-				...state,
-				cartItems : removeItemFromCart(state.cartItems, action.payload)
-			};
-		case CartActionTypes.CLEAR_ITEM_FROM_CART:
-			return {
-				...state,
-				cartItems : state.cartItems.filter((cartItem) => cartItem.id !== action.payload.id)
-			};
-
-		default:
-			return state;
-	}
+  switch (action.type) {
+    case CartActionTypes.TOGGLE_CART_HIDDEN:
+      return {
+        ...state,
+        hidden: !state.hidden
+      };
+    case CartActionTypes.ADD_ITEM:
+      return {
+        ...state,
+        // we spread our array values and any additional values added at the end will appear at the end of our array
+        cartItems: addItemToCart(state.cartItems, action.payload)
+      };
+    case CartActionTypes.REMOVE_ITEM:
+      return {
+        // spreading the state cause we want all the properties
+        ...state,
+        cartItems: removeItemFromCart(state.cartItems, action.payload)
+      };
+    case CartActionTypes.CLEAR_ITEM_FROM_CART:
+      return {
+        ...state,
+        cartItems: state.cartItems.filter(
+          cartItem => cartItem.id !== action.payload.id
+        )
+      };
+    case CartActionTypes.CLEAR_CART:
+      return {
+        ...state,
+        cartItems: []
+      };
+    default:
+      return state;
+  }
 };
 
 export default cartReducer;
